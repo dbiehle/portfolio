6fb7d77fea9a6f6072da882c4213ae50114b298d
Drew Biehle <drew.biehle@gmail.com>
2017-07-11 11:55:18 -0700

    Icons, normalizer, nav bar added and styled

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+/////////////////////////////////////////////////////////
+// TODO: make a constructor function for the projects
+//        include project name, screenshot, description of problem, and solution
+////////////////////////////////////////////////////////
+
+var Projects = function(projectName, screenshot, problem, solution, repoLink) {
+  this.projectName = projectName;
+  this.screenshot = 'images/screenshots/' + screenshot + '.jpg';
+  this.problem = problem;
+  this.solution = solution;
+  this.repoLink = repoLink;
+};
