44ea0a1f4ba094997e2ce9f73778c1e99e0d00fd
Drew Biehle <drew.biehle@gmail.com>
2017-07-12 10:39:45 -0700

    Html template cloned, projects rendering to the DOM

--- js/addprojects.js
+++ js/addprojects.js
@@ -9,6 +9,7 @@
 var addProjects = [
   {
     projectName:  'Choose Your Next Adventure',
+    createdOn:    '2017-06-30',
     screenshot:   'chooseHike',
     problem:      'Where can new Seattlites find their first hiking adventure? Where can long-time residents find new suggestions for hikes? Where can new hikers take their first steps?',
     solution:     'With a simple interface, CYNA tailors a hike to your needs. And when you finish with that amazing hike, come back and memorialize your adventure in your own private journal.',
@@ -16,6 +17,7 @@ var addProjects = [
   },
   {
     projectName:  'Is My Puppers Too Fat?',
+    createdOn:    '2017-03-15',
     screenshot:   'fatPupper',
     problem:      'What\'s a quick way to find out of my adorable dog is TOO overweight? If only there were a way to check.',
     solution:     'IMP2Fat provides a short and easy way to find out whether you\'re feeding your puppers too much, exercising your puppers enough, and giving your puppers enough scritches and smooches.',
@@ -23,6 +25,7 @@ var addProjects = [
   },
   {
     projectName:  'Where Did I Leave My Child?',
+    createdOn:    '2016-11-27',
     screenshot:   'lostKid',
     problem:      'Kids are wonderful but phones have all these cool games and fun ways to watch TV. So how can a parent or guardian keep up with where their kid went?',
     solution:     'Where Did I LMC is a fun way to use social media and our incessantly recording-everything culure to keep an eye (or eyes) on your kids at all times.',
--- js/app.js
+++ js/app.js
@@ -4,26 +4,31 @@
   TODO:
     - create a new JS file to modify the source data and render it to the DOM
         Estimated time: 1 hour
+        Actual time: about 1 hour
     - use jQuery to clone the example markup for each project
         Estimated time: 30 minutes
-    - Project prototype needs a .toHtml() function to add new data to the DOM
+        Actual time: about 30 minutes
+    - Project prototype needs a .toTheDom() function to add new data to the DOM
         Estimated time: 30 minutes
+        Actual time: about 30 minutes
 ************************************************************/
-var projects = [];
+var projectsArray = [];
 
 function Project (addProjectsObj) {
   this.projectName = addProjectsObj.projectName;
+  this.createdOn = addProjectsObj.createdOn;
   this.screenshot = 'images/screenshots/' + addProjectsObj.screenshot + '.jpg';
   this.problem = addProjectsObj.problem;
   this.solution = addProjectsObj.solution;
   this.repoLink = addProjectsObj.repoLink;
 }
 
-Project.prototype.toHtml = function() {
+Project.prototype.toTheDom = function() {
   var $newProject = $('article.template').clone();
   $newProject.removeClass('template');
 
   $newProject.find('p').first().text(this.projectName);
+  $newProject.find('time').html(parseInt((new Date() - new Date(this.createdOn))/60/60/24/1000) + ' days ago');
   $newProject.find('img').attr('src', this.screenshot);
   $newProject.find('div.problem').first().text(this.problem);
   $newProject.find('div.solution').first().text(this.solution);
@@ -31,3 +36,15 @@ Project.prototype.toHtml = function() {
 
   return $newProject;
 };
+
+addProjects.sort(function(a,b){
+  return ((new Date(b.createdOn)) - (new Date(a.createdOn)));
+});
+
+addProjects.forEach(function(projet){
+  projectsArray.push(new Project(projet));
+});
+
+projectsArray.forEach(function(projAppend){
+  $('#project-section').append(projAppend.toTheDom());
+});
