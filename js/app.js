83447a25f7b77896c303b25789949d2218495d7a
Drew Biehle <drew.biehle@gmail.com>
2017-07-18 10:55:06 -0700

    Data from AJAX request loading to page, also localStorage implemented

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var projectsArray = [];
-
-var projectFun = {
-  projectName: []
-};
-
 function Project (addProjectsObj) {
   this.projectName = addProjectsObj.projectName;
   this.createdOn= parseInt((new Date() - new Date(addProjectsObj.createdOn))/60/60/24/1000);
@@ -15,42 +9,44 @@ function Project (addProjectsObj) {
   this.repoLink = addProjectsObj.repoLink;
 }
 
+Project.all = [];
+
 Project.prototype.toTheDom = function() {
   var getProjectTemplate = $('#projectTemplate').html();
   var projectCompiler = Handlebars.compile(getProjectTemplate);
   return projectCompiler(this);
 };
 
-// need a $.getSON statement that gets the url for the JSON
-// also need a .then statement to handle what to do if successful
+Project.loadAll = function(rawData) {
+  rawData.sort(function(a,b){
+    return ((new Date(b.createdOn)) - (new Date(a.createdOn)));
+  });
+
+  rawData.forEach(function(projet){
+    Project.all.push(new Project(projet));
+  });
+
+  Project.all.forEach(function(projAppend){
+    $('#project-section').append(projAppend.toTheDom());
+  });
+}
+
 function getData() {
-  const url = 'js/addprojects.json';
-  $.getJSON(url).then(
+  $.getJSON('js/addprojects.json').then(
     function(data) {
-  console.log('success!')
-  // console.log(data)
-    projectsArray = data;
-  console.log(projectsArray)
+      localStorage.setItem('projectList', JSON.stringify(data));
+      Project.loadAll(data);
     }, function(err) {
-  console.log('error')
-  console.log(err)
+    console.error(err);
   }
   );
 }
 
 $(document).ready(function(){
-  getData();
+  if (localStorage.projectList) {
+    let retrievedData = JSON.parse(localStorage.projectList);
+    Project.loadAll(retrievedData);
+  } else {
+    getData();
+  }
 })
-
-
-// addProjects.sort(function(a,b){
-//   return ((new Date(b.createdOn)) - (new Date(a.createdOn)));
-// });
-//
-// addProjects.forEach(function(projet){
-//   projectsArray.push(new Project(projet));
-// });
-
-projectsArray.forEach(function(projAppend){
-  $('#project-section').append(projAppend.toTheDom());
-});
