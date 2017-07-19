3ba36a47f79cbb8a627dc61d5c686e9ada1d4408
Drew Biehle <drew.biehle@gmail.com>
2017-07-18 18:18:25 -0700

    Added ability to run from server.js using node

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+app.use(express.static('./public'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function() {
+  console.log('Listening on port ' + PORT);
+})
