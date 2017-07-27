47f1b6c341cb018d8e240f1686857fb64b25b30f
Drew Biehle <drew.biehle@gmail.com>
2017-07-27 11:12:08 -0700

    Added click handler to return functionality to the hamburger icon

--- public/js/views/projectview.js
+++ public/js/views/projectview.js
@@ -1,28 +1,19 @@
 'use strict';
 
-//Two click handlers when the site loads that show/hide elements from the navigation
-// $(document).ready(function() {
-//   $('.tab').click(function(event) {
-//     event.preventDefault();
-//     $('.tab-content, #project-section').show();
-//     $('.tab-content, #about-section').hide();
-//     var clickedValue = $(this).attr('data-content');
-//     $(`section[id = "${clickedValue}"]`).show();
-//   });
-//
-//   $('.icon-menu').click(function() {
-//     event.preventDefault();
-//     if ($('.main-nav ul').is(':visible')){
-//       $('.main-nav ul').hide();
-//     } else {
-//       $('.main-nav ul').show();
-//     }
-//   });
-//
-//   $('.main-nav .tab:first').click();
-// })
 
 (function(module){
+
+  $('.icon-menu').click(function() {
+    event.preventDefault();
+    if ($('.main-nav ul').is(':visible')){
+      $('.main-nav ul').hide();
+    } else {
+      $('.main-nav ul').show();
+    }
+  });
+
+  $('.main-nav .tab:first').click();
+
   const projectView = {
     initProjectPage: () => {
       $('#project-section').empty();
