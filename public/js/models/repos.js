21e697195f616f5cdb2ab9f625dae3cf8f15725c
Drew Biehle <drew.biehle@gmail.com>
2017-07-27 12:03:36 -0700

    Edited jQuery get method to simplify then and callback

--- public/js/models/repos.js
+++ public/js/models/repos.js
@@ -9,15 +9,16 @@ var app = app || {};
 
   repos.requestRepos = function(callback){
     $.get('/github/user/repos')
-    .then(function(data){
-      repos.all = data.map(repo => ({
-        name: repo.name,
-        html_url: repo.html_url,
-        description: repo.description,
-        lastUpdated: repo.updated_at
-      }));
-      callback();
-    })
+      .then(data => repos.all = data)
+      .then(callback);
   };
   module.repos = repos;
 })(app);
+
+// .then(function(data){
+//   repos.all = data.map(repo => ({
+//     name: repo.name,
+//     html_url: repo.html_url,
+//     description: repo.description,
+//     lastUpdated: repo.updated_at
+//   }));
