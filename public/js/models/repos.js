a5ed9b16f87a3e249937cae06b09f7f7308908cf
Drew Biehle <drew.biehle@gmail.com>
2017-07-28 16:35:35 -0700

    Edited callback in repos.js to repos.all array

--- public/js/models/repos.js
+++ public/js/models/repos.js
@@ -5,19 +5,19 @@ var app = app || {};
 (function(module){
   const repos = {};
 
-  // repos.all = [];
-// repos.all = data.map(repo => ({
+  repos.all = [];
 
   repos.requestRepos = function(callback){
     $.get('github/user/repos')
     .then(function(data){
-      var myMappedData = data.map(repo => ({
+      repos.all = data.map(repo => ({
+      // var myMappedData = data.map(repo => ({
         name: repo.name,
         html_url: repo.html_url,
         description: repo.description,
         lastUpdated: repo.updated_at
       }));
-      callback(myMappedData);
+      callback(repos.all);
     })
   };
 
