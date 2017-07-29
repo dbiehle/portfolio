476e99218bd87b525a4705a1f6a29f2ff7eaa7f5
Drew Biehle <drew.biehle@gmail.com>
2017-07-28 22:05:05 -0700

    Added ctx, next to adminController.index function

--- public/js/controller/admincontroller.js
+++ public/js/controller/admincontroller.js
@@ -5,11 +5,18 @@ var app = app || {};
 (function(module){
 
   const adminController = {};
-  adminController.index = () => {
+  // adminController.index = () => {
+  //   $('.tab-content').hide();
+  //   $('#stats-section').show();
+  //   app.adminView.initAdminPage();
+  //   app.repos.requestRepos(app.repoView.renderTheRepos);
+  // }
+  adminController.index = function(ctx, next){
     $('.tab-content').hide();
     $('#stats-section').show();
     app.adminView.initAdminPage();
     app.repos.requestRepos(app.repoView.renderTheRepos);
+    next();
   }
 
 // This is a test function to see if this could work and be called in routes.js
