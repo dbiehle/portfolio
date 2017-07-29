7a3a5c5f771205430c78a4437c6c87d966b74dca
Drew Biehle <drew.biehle@gmail.com>
2017-07-28 22:09:15 -0700

    Removed ctx, next from adminController.index to get app to work again

--- public/js/controller/admincontroller.js
+++ public/js/controller/admincontroller.js
@@ -5,21 +5,13 @@ var app = app || {};
 (function(module){
 
   const adminController = {};
-  // adminController.index = () => {
-  //   $('.tab-content').hide();
-  //   $('#stats-section').show();
-  //   app.adminView.initAdminPage();
-  //   app.repos.requestRepos(app.repoView.renderTheRepos);
-  // }
-  adminController.index = function(ctx, next){
+  adminController.index = () => {
     $('.tab-content').hide();
     $('#stats-section').show();
     app.adminView.initAdminPage();
     app.repos.requestRepos(app.repoView.renderTheRepos);
-    next();
   }
 
-// This is a test function to see if this could work and be called in routes.js
   adminController.findRepo = function(ctx, next) {
     let singleRepo = app.repos.all(repo => repo.name === ctx.params.name);
     app.repos = singleRepo;
